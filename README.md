daf216ab135f1ba110da63103d1cdd94e8d6c057
SleepDeprived0025 <sleeplessnight1909@gmail.com>
2024-09-16 16:32:29 +0200

    added logos, built partials and homepage

--- README.md
+++ README.md
@@ -5,6 +5,11 @@ A place to organize your thoughts 💭 in blocks 🧱
 This is being built with [Express](https://expressjs.com/) and [EJS](https://ejs.co/#about).
 You can view it by downloading the project and hosting it locally with something like [nodemon](https://www.npmjs.com/package/nodemon).
 
+## Update as of 16.09.2024
+
+- added page logos
+- finished building and styling partials and the home page
+
 ## Functionality to be made ⬇
 
 - Creating posts (thoughts) ❌
--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 import express from "express";
 import bodyParser from "body-parser";
-import ejs from "ejs";
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use('/static', express.static('public'));
 
-app.listen("/", port, () => {
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.listen(port, (req,res) => {
     console.log(`Server running on port ${port}`);
 })
